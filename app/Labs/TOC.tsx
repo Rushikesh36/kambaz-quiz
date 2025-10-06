aadffb39b1a974bc89ca8e04beb84eaf747ef499
Rushikesh Wani <rushikeshwani36@gmail.com>
2025-10-06 00:27:25 -0400

    Added github id as mentioned

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -19,7 +19,7 @@ export default function TOC() {
                 <NavLink href="/" as={Link}>Kambaz</NavLink>
             </NavItem>
             <NavItem>
-                <NavLink href="https://github.com/rushikesh36">My GitHub</NavLink>
+                <NavLink id="wd-github" href="https://github.com/rushikesh36">My GitHub</NavLink>
             </NavItem>
         </Nav>
     );
