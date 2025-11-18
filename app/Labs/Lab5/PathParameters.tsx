43b1c0869cbda4587d4642c90e2399e839292d43
Rushikesh Wani <rushikeshwani36@gmail.com>
2025-11-18 01:25:14 -0500

    final commit

--- app/Labs/Lab5/PathParameters.tsx
+++ app/Labs/Lab5/PathParameters.tsx
@@ -20,6 +20,7 @@ export default function PathParameters() {
                 href={`${HTTP_SERVER}/lab5/subtract/${a}/${b}`}>
                 Substract {a} - {b}
             </a>
+            
             <a
                 className="btn btn-success me-2"
                 id="wd-path-parameter-multiply"
