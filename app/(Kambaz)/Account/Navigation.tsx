81a2df61cb78eb2d5164d74153d7b0ce38220ede
Rushikesh Wani <rushikeshwani36@gmail.com>
2025-11-27 13:15:34 -0500

    fixed error

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -4,19 +4,41 @@ import { usePathname } from "next/navigation";
 import { Nav, NavItem, NavLink } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
+
+type AccountUser = {
+    role?: string;
+    // add other fields if you want (username, etc.)
+};
+
 export default function AccountNavigation() {
-    const { currentUser } = useSelector((state: RootState) => state.accountReducer);
+    const accountState = useSelector((state: RootState) => state.accountReducer);
+    const currentUser = accountState.currentUser as AccountUser | null;
+
     const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
     const pathname = usePathname();
+
     return (
         <Nav variant="pills">
-            {currentUser && "role" in currentUser && currentUser.role === "ADMIN" && (
-                <NavLink as={Link} href={`/Account/Users`} active={pathname.endsWith('Users')}> Users </NavLink>)}
+            {currentUser?.role === "ADMIN" && (
+                <NavLink
+                    as={Link}
+                    href="/Account/Users"
+                    active={pathname.endsWith("Users")}
+                >
+                    Users
+                </NavLink>
+            )}
 
             {links.map((link) => (
                 <NavItem key={link}>
-                    <NavLink as={Link} href={link} active={pathname.endsWith(link.toLowerCase())}>
-                        {link} </NavLink> </NavItem>
+                    <NavLink
+                        as={Link}
+                        href={`/Account/${link}`}
+                        active={pathname.toLowerCase().endsWith(link.toLowerCase())}
+                    >
+                        {link}
+                    </NavLink>
+                </NavItem>
             ))}
         </Nav>
     );
